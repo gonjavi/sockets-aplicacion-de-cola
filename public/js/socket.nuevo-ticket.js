e0851adf9e44612b1ad0f456f7f78a9ddca6f409
manci <javiercaliescali@gmail.com>
2021-01-13 10:26:20 -0500

    conectar asignacion de tickets con pantalla de cola

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,23 @@
+// comando para establaecer la conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket')
+
+socket.on('connect', function() {
+  console.log('Conenctado al servidor');
+});
+
+socket.on('disconnect', function() {
+  console.log('Desconectado');
+});
+
+// on ' estado actual
+socket.on('estadoActual', function(resp) {
+  label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+  socket.emit('siguienteTicket', null, function(siguienteTicket) {
+    label.text(siguienteTicket); 
+  });
+});
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -15,6 +15,26 @@ io.on('connection', (client) => {
 
   // emitir evento estado actual
   client.emit('estadoActual', {
-    actual: ticketControl.getUltimoTicket()
+    actual: ticketControl.getUltimoTicket(),
+    ultimos4: ticketControl.getUltimos4()
   });    
+  
+  client.on('atenderTicket', (data, callback) => {
+    if (!data.escritorio) {
+      return callback({
+        err: true,
+        mensaje: 'El escritorio es necesario'
+      });
+    }
+
+    let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+    callback(atenderTicket);
+
+    // actualizar/ notificar cambios en los ultimos 4
+    client.broadcast.emit('ultimos4', {
+      ultimos4: ticketControl.getUltimos4()
+    });
+  });
+
 });
